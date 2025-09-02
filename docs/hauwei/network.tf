38aa6da3eaba1965d4d9f900dd32beb7a7271c88
JohnnyFoulds <hfoulds@gmail.com>
2025-09-02 14:00:27 +0200

    SSH security group rule

--- docs/hauwei/network.tf
+++ docs/hauwei/network.tf
@@ -24,6 +24,16 @@ resource "huaweicloud_networking_secgroup_rule" "secgroup_rule_http" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.secgroup.id
+}
+
+resource "huaweicloud_networking_secgroup_rule" "secgroup_rule_ssh" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
   port_range_min    = 22
   port_range_max    = 22
   remote_ip_prefix  = "0.0.0.0/0"
